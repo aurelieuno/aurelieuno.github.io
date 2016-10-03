801659791cd4369f229dff8f8b9bd75eaac05c50
aurelieuno1 <aurelie.lebec@gmai.com>
2016-10-03 22:41:21 +0000

    * archive bootcamp one

--- projects/function-master/algorithms/DNApairs.1.js
+++
@@ -1,9 +0,0 @@
-function drop(arr, func) {
-  for(var i=0;i<arr.length;i++){
-    if(func(arr[i])===true) {return arr.slice(i);
-     } else { return arr.slice(i+1);}
-  }
- 
-}
-
-console.log(drop([1, 2, 3, 4], function(n) {return n > 5;}));
